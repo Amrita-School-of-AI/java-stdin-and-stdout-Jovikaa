56a38e81382f49146e69032d8fe47400ebf023cd
Jovikaa <jovikabala5@gmail.com>
2025-03-04 14:10:25 +0530

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -6,13 +6,12 @@ public class App {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter three integers:"); 
+        
 
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
 
-        System.out.println("You entered:");
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
